15a891fe6121ad56b9e4602267e76d19ed59a4c3
YTRellVr <R4llalt@gmail.com>
2025-05-13 09:52:54 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,21 +1,23 @@
-//SetUp
+// SetUp
 scene.setBackgroundImage(assets.image`Background`)
 
 
-//Sprite Set Up
+// Sprite Set Up
+
+
 let Player = sprites.create(assets.image`Player`, SpriteKind.Player)
 Player.setStayInScreen(true)
 Player.setScale(1.5, ScaleAnchor.Middle)
 
-//Movement Set Up
+
+// Movement Set Up
+
+
 controller.moveSprite(Player)
-controller.right.onEvent(ControllerButtonEvent.Pressed, function() {
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+
+  
     
-    let Player_RightMove = animation.createAnimation(50, 1000)
-            
-        
-      
-    
-    
+animation.runImageAnimation(Player, [], 500, false)
 
 })
