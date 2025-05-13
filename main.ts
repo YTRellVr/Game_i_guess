6f9a6d5875bb78559b481178b2a2ec80a9c10fea
YTRellVr <R4llalt@gmail.com>
2025-05-13 10:51:01 -0400

    Update pxt.json, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
 // SetUp
-scene.setBackgroundImage(assets.image`Background`)
+scene.setBackgroundImage(assets.image`PathWayStart`)
+forever(function() {
+    console.log(null)
 
+})
 
 // Sprite Set Up
 
@@ -8,7 +11,17 @@ scene.setBackgroundImage(assets.image`Background`)
 let Player = sprites.create(assets.image`Player`, SpriteKind.Player)
 Player.setStayInScreen(true)
 Player.setScale(1.5, ScaleAnchor.Middle)
+let Hitbox_Up = sprites.create(assets.image`Hitbox up start`, SpriteKind.Food)
+Hitbox_Up.setScale(1.5, ScaleAnchor.Middle)
+Hitbox_Up.setPosition(71, 7)
 
+//next area code
+    
+if (true) {
+        
+    Player.overlapsWith(Hitbox_Up)
+
+    }
 
 // Movement Set Up
 
