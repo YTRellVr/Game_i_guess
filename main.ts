70575b0553c8667ae576de878790f7f955851045
YTRellVr <R4llalt@gmail.com>
2025-05-13 13:58:57 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,71 +1,51 @@
 // SetUp
 scene.setBackgroundImage(assets.image`PathWayStart`)
-forever(function() {
 
-})
+
+ //next area code
+
+
 
 // Sprite Set Up
 let Computer = sprites.create(assets.image`Computer World select`, SpriteKind.Food)
 Computer.setScale(1.3, ScaleAnchor.Middle)
 Computer.setPosition(21, 67)
-
 let Player = sprites.create(assets.image`PlayerFront`, SpriteKind.Player)
+Player = sprites.create(assets.image`PlayerFront`, SpriteKind.Player)
 Player.setStayInScreen(true)
 Player.setScale(1.5, ScaleAnchor.Middle)
-characterAnimations.setCharacterAnimationsEnabled(Player, true)
 
+forever(function () {
+	
+})
 
-//next area code
-    
-
-    
-
-
-
-// Movement Set Up
-
+//Movement Set Up
 controller.moveSprite(Player)
-  
-    
 
-controller.player1.onButtonEvent(ControllerButton.Right, ControllerButtonEvent.Pressed, function() {
-    
-    Player.setImage(assets.image`MoveRight1`)
-    pause(300)
-    Player.setImage(assets.image`MoveRight2`)
-    pause(300)
-   
-
-    controller.player1.onButtonEvent(ControllerButton.Right, ControllerButtonEvent.Released, function () {
-
-        Player.setImage(assets.image`MoveRight1`)
-        pause(600)
-        Player.setImage(assets.image`MoveRight1`)
+//MoveUp
 
 
-    })
-
-})
-
-controller.player1.onButtonEvent(ControllerButton.Down, ControllerButtonEvent.Pressed, function () {
-while (true) {
-    
-}
-    Player.setImage(assets.image`PlayerFront`)
-    pause(300)
-    Player.setImage(assets.image`MoveDown2`)
-    pause(300)
+//MoveLeft
 
 
-    controller.player1.onButtonEvent(ControllerButton.Down, ControllerButtonEvent.Released, function () {
 
+//MoveDown
+forever(function () {
+    while (controller.player1.isPressed(ControllerButton.Down)) {
         Player.setImage(assets.image`PlayerFront`)
-
-
-
-    })
-
+        pause(300)
+        Player.setImage(assets.image`MoveDown2`)
+        pause(300)
+    }
 })
 
+forever(function () {
+    while (controller.player1.isPressed(ControllerButton.Down)) {
+        Player.setImage(assets.image`PlayerFront`)
+        pause(300)
+        Player.setImage(assets.image`MoveDown2`)
+        pause(300)
+    }
+})
 
-
+//MoveRight
