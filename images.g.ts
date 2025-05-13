09272daea15972b8a4c7e49a6b3f57ccfb738ac4
YTRellVr <R4llalt@gmail.com>
2025-05-13 13:09:09 -0400

    Update main.ts, images.g.jres, images.g.ts

--- images.g.ts
+++ images.g.ts
@@ -3,25 +3,6 @@ namespace myImages {
 
     helpers._registerFactory("image", function(name: string) {
         switch(helpers.stringTrim(name)) {
-            case "image1":
-            case "Player":return img`
-. . . . . . f f f f . . . . . . 
-. . . . f f f 2 2 f f f . . . . 
-. . . f f f 2 2 2 2 f f f . . . 
-. . f f f e e e e e e f f f . . 
-. . f f e 2 2 2 2 2 2 e e f . . 
-. . f e 2 f f f f f f 2 e f . . 
-. . f f f f e e e e f f f f . . 
-. f f e f b f 4 4 f b f e f f . 
-. f e e 4 1 f d d f 1 4 e e f . 
-. . f e e d d d d d d e e f . . 
-. . . f e e 4 4 4 4 e e f . . . 
-. . e 4 f 2 2 2 2 2 2 f 4 e . . 
-. . 4 d f 2 2 2 2 2 2 f d 4 . . 
-. . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
-. . . . . f f f f f f . . . . . 
-. . . . . f f . . f f . . . . . 
-`;
             case "image2":
             case "PathWayStart":return img`
 7777777777777777777777777777777777777777777777777777744444444444444444444444444444444444477777777777777777777777777777777777777777777777777777777777777777777777
@@ -179,6 +160,63 @@ ddff7ff77ff7f77ff7f7f7f7ff7fffdd
 ddffffffffffffffffffffffffffffdd
 dddddddddddddddddddddddddddddddd
 dddddddddddddddddddddddddddddddd
+`;
+            case "image3":
+            case "MoveRight2":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . f f f f f f . . . . 
+. . . . f f e e e e f 2 f . . . 
+. . . f f e e e e f 2 2 2 f . . 
+. . . f e e e f f e e e e f . . 
+. . . f f f f e e 2 2 2 2 e f . 
+. . . f e 2 2 2 f f f f e 2 f . 
+. . f f f f f f f e e e f f f . 
+. . f f e 4 4 e b f 4 4 e e f . 
+. . f e e 4 d 4 1 f d d e f . . 
+. . . f e e e e e d d d f . . . 
+. . . . . f 4 d d e 4 e f . . . 
+. . . . . f e d d e 2 2 f . . . 
+. . . . f f f e e f 5 5 f f . . 
+. . . . f f f f f f f f f f . . 
+. . . . . f f . . . f f f . . . 
+`;
+            case "image7":
+            case "MoveRight1":return img`
+. . . . . . f f f f f f . . . . 
+. . . . f f e e e e f 2 f . . . 
+. . . f f e e e e f 2 2 2 f . . 
+. . . f e e e f f e e e e f . . 
+. . . f f f f e e 2 2 2 2 e f . 
+. . . f e 2 2 2 f f f f e 2 f . 
+. . f f f f f f f e e e f f f . 
+. . f f e 4 4 e b f 4 4 e e f . 
+. . f e e 4 d 4 1 f d d e f . . 
+. . . f e e e 4 d d d d f . . . 
+. . . . f f e e 4 4 4 e f . . . 
+. . . . . 4 d d e 2 2 2 f . . . 
+. . . . . e d d e 2 2 2 f . . . 
+. . . . . f e e f 4 5 5 f . . . 
+. . . . . . f f f f f f . . . . 
+. . . . . . . f f f . . . . . . 
+`;
+            case "image8":
+            case "PlayerFront":return img`
+. . . . . . f f f f . . . . . . 
+. . . . f f f 2 2 f f f . . . . 
+. . . f f f 2 2 2 2 f f f . . . 
+. . f f f e e e e e e f f f . . 
+. . f f e 2 2 2 2 2 2 e e f . . 
+. . f e 2 f f f f f f 2 e f . . 
+. . f f f f e e e e f f f f . . 
+. f f e f b f 4 4 f b f e f f . 
+. f e e 4 1 f d d f 1 4 e e f . 
+. . f e e d d d d d d e e f . . 
+. . . f e e 4 4 4 4 e e f . . . 
+. . e 4 f 2 2 2 2 2 2 f 4 e . . 
+. . 4 d f 2 2 2 2 2 2 f d 4 . . 
+. . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+. . . . . f f f f f f . . . . . 
+. . . . . f f . . f f . . . . . 
 `;
         }
         return null;
--- main.ts
+++ main.ts
@@ -9,7 +9,7 @@ let Computer = sprites.create(assets.image`Computer World select`, SpriteKind.Fo
 Computer.setScale(1.3, ScaleAnchor.Middle)
 Computer.setPosition(21, 67)
 
-let Player = sprites.create(assets.image`Player`, SpriteKind.Player)
+let Player = sprites.create(assets.image`PlayerFront`, SpriteKind.Player)
 Player.setStayInScreen(true)
 Player.setScale(1.5, ScaleAnchor.Middle)
 characterAnimations.setCharacterAnimationsEnabled(Player, true)
@@ -25,11 +25,24 @@ characterAnimations.setCharacterAnimationsEnabled(Player, true)
 // Movement Set Up
 
 controller.moveSprite(Player)
-controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
-    characterAnimations.runFrames(Player, [], 500, characterAnimations.rule(Predicate.NotMoving))
   
-    animation.runImageAnimation(Player, [], 500, false)
-    animation.attachAnimation(Player, null)
     
 
+controller.player1.onButtonEvent(ControllerButton.Right, ControllerButtonEvent.Pressed, function() {
+    
+    Player.setImage(assets.image`MoveRight1`)
+    pause(500)
+    Player.setImage(assets.image`MoveRight2`)
+    pause(500)
+   
+
+    controller.player1.onButtonEvent(ControllerButton.Right, ControllerButtonEvent.Released, function () {
+
+        Player.setImage(assets.image`MoveRight1`)
+
+
+
+    })
+
 })
+
