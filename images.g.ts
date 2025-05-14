2ee0f81b6e35ce6e81891c0e110913a258b78f14
YTRellVr <R4llalt@gmail.com>
2025-05-14 09:40:05 -0400

    Update main.ts, images.g.jres, images.g.ts

--- images.g.ts
+++ images.g.ts
@@ -161,25 +161,6 @@ ddffffffffffffffffffffffffffffdd
 dddddddddddddddddddddddddddddddd
 dddddddddddddddddddddddddddddddd
 `;
-            case "image7":
-            case "MoveRight1":return img`
-. . . . . . f f f f f f . . . . 
-. . . . f f e e e e f 2 f . . . 
-. . . f f e e e e f 2 2 2 f . . 
-. . . f e e e f f e e e e f . . 
-. . . f f f f e e 2 2 2 2 e f . 
-. . . f e 2 2 2 f f f f e 2 f . 
-. . f f f f f f f e e e f f f . 
-. . f f e 4 4 e b f 4 4 e e f . 
-. . f e e 4 d 4 1 f d d e f . . 
-. . . f e e e 4 d d d d f . . . 
-. . . . f f e e 4 4 4 e f . . . 
-. . . . . 4 d d e 2 2 2 f . . . 
-. . . . . e d d e 2 2 2 f . . . 
-. . . . . f e e f 4 5 5 f . . . 
-. . . . . . f f f f f f . . . . 
-. . . . . . . f f f . . . . . . 
-`;
             case "image8":
             case "PlayerFront":return img`
 . . . . . . f f f f . . . . . . 
@@ -219,6 +200,44 @@ dddddddddddddddddddddddddddddddd
 . . . . f f f . . . . . . . . . 
 `;
             case "image1":
+            case "MoveLeft2":return img`
+. . . . . . . . . . . . . . . . 
+. . . . f f f f f f . . . . . . 
+. . . f 2 f e e e e f f . . . . 
+. . f 2 2 2 f e e e e f f . . . 
+. . f e e e e f f e e e f . . . 
+. f e 2 2 2 2 e e f f f f . . . 
+. f 2 e f f f f 2 2 2 e f . . . 
+. f f f e e e f f f f f f f . . 
+. f e e 4 4 f b e 4 4 e f f . . 
+. . f e d d f 1 4 d 4 e e f . . 
+. . . f d d d d 4 e e e f . . . 
+. . . f e 4 4 4 e d d 4 . . . . 
+. . . f 2 2 2 2 e d d e . . . . 
+. . f f 5 5 4 4 f e e f . . . . 
+. . f f f f f f f f f f . . . . 
+. . . f f f . . . f f . . . . . 
+`;
+            case "image7":
+            case "MoveLeft1":return img`
+. . . . f f f f f f . . . . . . 
+. . . f 2 f e e e e f f . . . . 
+. . f 2 2 2 f e e e e f f . . . 
+. . f e e e e f f e e e f . . . 
+. f e 2 2 2 2 e e f f f f . . . 
+. f 2 e f f f f 2 2 2 e f . . . 
+. f f f e e e f f f f f f f . . 
+. f e e 4 4 f b e 4 4 e f f . . 
+. . f e d d f 1 4 d 4 e e f . . 
+. . . f d d d d 4 e e e f . . . 
+. . . f e 4 4 4 e e f f . . . . 
+. . . f 2 2 2 e d d 4 . . . . . 
+. . . f 2 2 2 e d d e . . . . . 
+. . . f 5 5 4 f e e f . . . . . 
+. . . . f f f f f f . . . . . . 
+. . . . . . f f f . . . . . . . 
+`;
+            case "image4":
             case "MoveRight2":return img`
 . . . . . . . . . . . . . . . . 
 . . . . . . f f f f f f . . . . 
@@ -236,6 +255,63 @@ dddddddddddddddddddddddddddddddd
 . . . . f e e f 4 4 5 5 f f . . 
 . . . . f f f f f f f f f f . . 
 . . . . . f f . . . f f f . . . 
+`;
+            case "image6":
+            case "MoveRight1":return img`
+. . . . . . f f f f f f . . . . 
+. . . . f f e e e e f 2 f . . . 
+. . . f f e e e e f 2 2 2 f . . 
+. . . f e e e f f e e e e f . . 
+. . . f f f f e e 2 2 2 2 e f . 
+. . . f e 2 2 2 f f f f e 2 f . 
+. . f f f f f f f e e e f f f . 
+. . f f e 4 4 e b f 4 4 e e f . 
+. . f e e 4 d 4 1 f d d e f . . 
+. . . f e e e 4 d d d d f . . . 
+. . . . f f e e 4 4 4 e f . . . 
+. . . . . 4 d d e 2 2 2 f . . . 
+. . . . . e d d e 2 2 2 f . . . 
+. . . . . f e e f 4 5 5 f . . . 
+. . . . . . f f f f f f . . . . 
+. . . . . . . f f f . . . . . . 
+`;
+            case "image9":
+            case "MoveUp2":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . f f f f . . . . . . 
+. . . . f f e e e e f f . . . . 
+. . . f e e e f f e e e f . . . 
+. . . f f f f 2 2 f f f f . . . 
+. . f f e 2 e 2 2 e 2 e f f . . 
+. . f e 2 f 2 f f f 2 f e f . . 
+. . f f f 2 f e e 2 2 f f f . . 
+. . f e 2 f f e e 2 f e e f . . 
+. f f e f f e e e f e e e f f . 
+. f f e e e e e e e e e e f f . 
+. . . f e e e e e e e e f . . . 
+. . . e f f f f f f f f 4 e . . 
+. . . 4 f 2 2 2 2 2 e d d 4 . . 
+. . . e f f f f f f e e 4 . . . 
+. . . . f f f . . . . . . . . . 
+`;
+            case "image10":
+            case "MoveUp1":return img`
+. . . . . . f f f f . . . . . . 
+. . . . f f e e e e f f . . . . 
+. . . f e e e f f e e e f . . . 
+. . f f f f f 2 2 f f f f f . . 
+. . f f e 2 e 2 2 e 2 e f f . . 
+. . f e 2 f 2 f f 2 f 2 e f . . 
+. . f f f 2 2 e e 2 2 f f f . . 
+. f f e f 2 f e e f 2 f e f f . 
+. f e e f f e e e e f e e e f . 
+. . f e e e e e e e e e e f . . 
+. . . f e e e e e e e e f . . . 
+. . e 4 f f f f f f f f 4 e . . 
+. . 4 d f 2 2 2 2 2 2 f d 4 . . 
+. . 4 4 f 4 4 4 4 4 4 f 4 4 . . 
+. . . . . f f f f f f . . . . . 
+. . . . . f f . . f f . . . . . 
 `;
         }
         return null;
--- main.ts
+++ main.ts
@@ -11,7 +11,6 @@ let Computer = sprites.create(assets.image`Computer World select`, SpriteKind.Fo
 Computer.setScale(1.3, ScaleAnchor.Middle)
 Computer.setPosition(21, 67)
 let Player = sprites.create(assets.image`PlayerFront`, SpriteKind.Player)
-Player = sprites.create(assets.image`PlayerFront`, SpriteKind.Player)
 Player.setStayInScreen(true)
 Player.setScale(1.5, ScaleAnchor.Middle)
 
@@ -23,29 +22,56 @@ forever(function () {
 controller.moveSprite(Player)
 
 //MoveUp
-
+forever(function () {
+    while (controller.player1.isPressed(ControllerButton.Up)) {
+        Player.setImage(assets.image`MoveUp2`)
+        pause(300)
+        Player.setImage(assets.image`MoveUp1`)
+        pause(300)
+    }
+})
+controller.up.onEvent(ControllerButtonEvent.Released, function () {
+    Player.setImage(assets.image`MoveUp1`)
+})
 
 //MoveLeft
-
+forever(function () {
+    while (controller.player1.isPressed(ControllerButton.Left)) {
+        Player.setImage(assets.image`MoveLeft2`)
+        pause(300)
+        Player.setImage(assets.image`MoveLeft1`)
+        pause(300)
+    }
+})
+controller.left.onEvent(ControllerButtonEvent.Released, function () {
+    Player.setImage(assets.image`MoveLeft1`)
+})
 
 
 //MoveDown
 forever(function () {
     while (controller.player1.isPressed(ControllerButton.Down)) {
-        Player.setImage(assets.image`PlayerFront`)
-        pause(300)
         Player.setImage(assets.image`MoveDown2`)
         pause(300)
+        Player.setImage(assets.image`PlayerFront`)
+        pause(300)
     }
+})
+controller.down.onEvent(ControllerButtonEvent.Released, function() {
+    Player.setImage(assets.image`PlayerFront`)
 })
 
-forever(function () {
-    while (controller.player1.isPressed(ControllerButton.Down)) {
-        Player.setImage(assets.image`PlayerFront`)
-        pause(300)
-        Player.setImage(assets.image`MoveDown2`)
-        pause(300)
-    }
-})
+
 
 //MoveRight
+forever(function () {
+    while (controller.player1.isPressed(ControllerButton.Right)) {
+        Player.setImage(assets.image`MoveRight2`)
+        pause(300)
+        Player.setImage(assets.image`MoveRight1`)
+        pause(300)
+    }
+})
+controller.right.onEvent(ControllerButtonEvent.Released, function() {
+    Player.setImage(assets.image`MoveRight1`)
+})
